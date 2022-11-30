4dd63223bce1517998597756b44785b85e12fbf8
user <jarvizz76@gmail.com>
2022-11-30 16:28:49 +0300

    task 1 l3

---
+++ Lesson 3 Task 1.py
@@ -0,0 +1,10 @@
+text=input('Put name: ')
+text=text.replace(' ', '-')
+print(text)
+
+t = input()
+l = t.split()
+s1 = '-'.join(l)
+print(s1)
+
+
---
+++ Lesson 3 Task 2.py
@@ -0,0 +1,4 @@
+x=input('put number: ')
+c=input('put number: ')
+g=input('put number: ')
+print(word)
