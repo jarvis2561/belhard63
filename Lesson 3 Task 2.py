a0af5e334e0f9156e3c8d869229224b436c7aa5d
user <jarvizz76@gmail.com>
2022-12-02 09:55:10 +0300

    task 2 l3

--- Lesson 3 Task 2.py
+++ Lesson 3 Task 2.py
@@ -1,4 +1,8 @@
-x=input('put number: ')
-c=input('put number: ')
-g=input('put number: ')
-print(word)
+l1=float(input('Put number: '))
+l2=float(input('Put number: '))
+l3=float(input('Put number: '))
+l4 = (float(l1+l2+l3))
+l5=float(l4/3)
+print(round(l4,3))
+print(round(l5,3))
+
