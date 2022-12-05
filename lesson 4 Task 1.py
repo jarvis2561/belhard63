1d04968feb4ec876a56aeddd82ff54e9a877e957
user <jarvizz76@gmail.com>
2022-12-05 22:53:40 +0300

    task 1 l4

--- lesson 4 Task 1.py
+++ lesson 4 Task 1.py
@@ -5,13 +5,18 @@
 # numbers5=[i**5 for i in range (2, 3 )]
 # numbers6=numbers+numbers2+numbers3+numbers4+numbers5
 # print(numbers6)
-p = int(input("Показатель степени: "))
-n = int(input("Предел: "))
 
-i = 2
-while i ** p <= n:
-    print(i ** p, end=' ')
-    i += 1
+# p = int(input("Показатель степени: "))
+# n = int(input("Предел: "))
+#
+# i = 2
+# while i ** p <= n:
+#     print(i ** p, end=' ')
+#     i += 1
+#
+# print("\nПоследнее число,"
+#       " возведенное в степень:", i - 1)
 
-print("\nПоследнее число,"
-      " возведенное в степень:", i - 1)
+a=int(input('Put number: '))
+a=[2**i for i in range(1,1+4)]
+print(a)
