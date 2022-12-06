fda616ba11e7f04504e375db4d3e596b2b9c99bf
user <jarvizz76@gmail.com>
2022-12-06 23:20:56 +0300

    task 4 l4

---
+++ Lesson 4 Task 4.py
@@ -0,0 +1,3 @@
+v= int(input('v: '))
+vd={i: {'name': input('name: ') ,'email': input('email: ')} for i in range(2)}
+print(vd)
--- lesson 4 Task 1.py
+++ lesson 4 Task 1.py
@@ -1,22 +1,3 @@
-# numbers=[i**1 for i in range (2, 3 )]
-# numbers2=[i**2 for i in range (2, 3 )]
-# numbers3=[i**3 for i in range (2, 3 )]
-# numbers4=[i**4 for i in range (2, 3 )]
-# numbers5=[i**5 for i in range (2, 3 )]
-# numbers6=numbers+numbers2+numbers3+numbers4+numbers5
-# print(numbers6)
-
-# p = int(input("Показатель степени: "))
-# n = int(input("Предел: "))
-#
-# i = 2
-# while i ** p <= n:
-#     print(i ** p, end=' ')
-#     i += 1
-#
-# print("\nПоследнее число,"
-#       " возведенное в степень:", i - 1)
-
 a=int(input('Put number: '))
 a=[2**i for i in range(1,1+4)]
 print(a)
