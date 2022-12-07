e70bf21251851afc1b8a43f2ce336fa9284e8331
user <jarvizz76@gmail.com>
2022-12-07 17:23:48 +0300

    lesson 5 task1

---
+++ Lesson 5 Task 2.py
@@ -0,0 +1,3 @@
+number=float(input('Enter number: '))
+if number.isdigit:
+    {i for i do=float(input('What action do you want to take?: ')) }
---
+++ Lesson 5 task 1.py
@@ -0,0 +1,12 @@
+# a=int(input('vvedite chislo: '))
+# if a % 5 and a > 24 :
+#     print(a)
+# else: print('err')
+
+i = k = 0
+while i!=5:
+    if k%5 and k>5:
+        print(k)
+        i+=1
+    k+=1
+
