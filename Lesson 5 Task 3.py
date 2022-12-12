2e7dd21d7227e7a7f7cb80d308be2e0b67dd731e
user <jarvizz76@gmail.com>
2022-12-12 18:07:20 +0300

    lesson 5 task 3

--- Lesson 5 Task 3.py
+++ Lesson 5 Task 3.py
@@ -1,4 +1,9 @@
-c = 2
-while c<=70:
-    c+=2
-    print(c)
+n = int(input('n: ' ))
+for i in range(2, n + 1, 10):
+     for j in range(i, i+9, 2):
+         if j <= n:
+             print(j, end=' ')
+         else:
+             break
+     print()
+
